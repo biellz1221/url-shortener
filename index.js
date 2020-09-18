e51ba992effa715185c1290ada23e79a7ded5183
Gabriel Baptista <biellz1221@gmail.com>
2020-09-17 23:30:59 -0300

    deploy api

--- index.js
+++ index.js
@@ -1,39 +1,107 @@
-
-const express = require('express')
-const cors = require('cors')
-const helmet = require('helmet')
-const morgan = require('morgan')
-const { nanoid } = require('nanoid')
-// const yup = require('yup')
-// const monk = require('monk')
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const { nanoid } = require('nanoid');
+const yup = require('yup');
+const monk = require('monk');
 
 const app = express();
-app.use(helmet());
-app.use(morgan('tiny'))
+
+require('dotenv').config();
+
+const db = monk('mongodb+srv://admin:CavaloFesteiro2020,@cluster0.byqos.gcp.mongodb.net/urls?retryWrites=true&w=majority');
+
+const urls = db.get('urls');
+urls.createIndex({ slug: 1 }, { unique: true });
+
+app.use(
+	helmet({
+		contentSecurityPolicy: false,
+	})
+);
+app.use(morgan('tiny'));
 app.use(cors());
 app.use(express.json());
-app.use(express.static('./public'))
+app.use(express.static('./public'));
+
+const schema = yup.object().shape({
+	slug: yup
+		.string()
+		.trim()
+		.matches(/[\w\-]/i),
+	url: yup.string().trim().url().required(),
+});
 
 app.get('/', (req, res) => {
-    res.json({
-        message: 'tes.te'
-    })
-})
+	res.json({
+		message: 'tes.te',
+	});
+});
 
-app.get('/:id', (req, res) => {
-    //TODO: redirect
-})
+app.get('/:id', async (req, res, next) => {
+	const { id: slug } = req.params;
+	try {
+		const url = await urls.findOne({ slug });
+		if (url) {
+			res.redirect(url.url);
+		}
+		res.redirect(`/?error=${slug} not found`);
+	} catch (e) {
+		res.redirect(`/?error=Link not found`);
+	}
+});
 
-app.post('/url', (req, res) => {
-    //TODO: create
-})
+app.post('/url', async (req, res, next) => {
+	let { slug, url } = req.body;
+	try {
+		await schema.validate({
+			slug,
+			url,
+		});
+		if (!slug) {
+			slug = nanoid(5);
+		}
+		// else {
+		//     const existing = await urls.findOne({ slug });
+		//     if(existing) {
+		//         throw new Error('Slug in use.')
+		//     }
+		// }
+		slug = slug.toLowerCase();
+		const newURL = {
+			url,
+			slug,
+			clicks: 0,
+		};
+		const created = await urls.insert(newURL);
+		res.json(created);
+	} catch (e) {
+		if (e.message.startsWith('E11000')) {
+			e.message = 'Slug is in use';
+		}
+		next(e);
+	}
+});
+
+app.use((error, req, res, next) => {
+	if (error.status) {
+		res.status(error.status);
+	} else {
+		res.status(500);
+	}
+	res.json({
+		message: error.message,
+		stack: process.env.NODE_ENV === 'production' ? 'Nope' : error.stack,
+	});
+});
 
 app.get('/url/:id', (req, res) => {
-    //TODO: return info on the URL
-})
+	//TODO: return info on the URL
+});
 
-const port = process.env.PORT || 1337
+const port = process.env.PORT || 1337;
 
 app.listen(port, () => {
-    console.log(`Listening at http://localhost:${port}`)
-})
+	console.log(`Listening at http://localhost:${port}`);
+});
---
+++ public/app.js
@@ -0,0 +1,16 @@
+const app = new Vue({
+	el: '#app',
+	data() {
+		return {
+			url: 'test',
+			slug: 'test',
+			created: 'test',
+		};
+	},
+	methods: {
+		shorten() {},
+	},
+});
+
+//admin
+//CavaloFesteiro2020,
